b2a72aa68f8bd2ae88e299b7e16cd3d0cf1f6a5b
Harshitcoding <khandelwalharshit96@gmail.com>
2025-02-02 16:25:04 +0530

    fixing the code dynamic routes

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -1,22 +1,14 @@
-
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(
-  request: Request,
-  { params }: { params: Promise<{ id: string }>  }
-) {
+export async function GET(request: Request, context: { params: { id: string } }) {
   try {
-    // @ts-ignore
-    const userId = parseInt(params.id);
+    const userId = parseInt(context.params.id);
 
     if (isNaN(userId)) {
-      return NextResponse.json(
-        { message: 'Invalid post ID' },
-        { status: 400 }
-      );
+      return NextResponse.json({ message: 'Invalid post ID' }, { status: 400 });
     }
 
     const post = await prisma.post.findUnique({
@@ -26,19 +18,13 @@ export async function GET(
     });
 
     if (!post) {
-      return NextResponse.json(
-        { message: 'Post not found' },
-        { status: 404 }
-      );
+      return NextResponse.json({ message: 'Post not found' }, { status: 404 });
     }
 
     return NextResponse.json(post, { status: 200 });
   } catch (error) {
     console.error('Error fetching post:', error);
-    return NextResponse.json(
-      { message: 'Error fetching post' },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: 'Error fetching post' }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
