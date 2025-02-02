e269aed134c3e5b1875ad407b69df5a24ee573ff
Harshitcoding <khandelwalharshit96@gmail.com>
2025-02-02 15:01:24 +0530

    fixing the error

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function GET(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { id: string } }
 ) {
   try {
