4afbb78c64e3c25fe5658ba053e201db006b6553
Harshitcoding <khandelwalharshit96@gmail.com>
2025-02-03 11:12:37 +0530

    added beautiful button

--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -1,27 +1,32 @@
-import { UserButton } from "@clerk/nextjs"
-import Image from "next/image"
-import Link from "next/link"
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between w-full px-10 py-4 bg-purple-600 ">
+    <div className="flex items-center justify-between w-full px-10 py-4 bg-purple-600">
       <div>
-      <Link href={'/'}>
-      <Image src={'/stubhub.png'} alt="logo" height={100} width={100} className="bg-black"/>
-      </Link>
+        <Link href="/">
+          <Image
+            src="/stubhub.png"
+            alt="logo"
+            height={100}
+            width={100}
+            className="bg-black"
+          />
+        </Link>
       </div>
-      <div className="text-2xl font-bold items-center flex gap-2">
-       <div>
-       <Link href={'/new'}>
-       Post
-       </Link>
-       </div>
-        <div>
-            <UserButton/>
-       </div>
-       
+      <div className="text-2xl font-bold flex items-center gap-4">
+        <Link
+          href="/new"
+          className="px-2 py-1 bg-white text-purple-600 font-semibold rounded-lg shadow-md transition duration-300 hover:bg-purple-700 hover:text-white hover:shadow-lg active:scale-95"
+        >
+          Post
+        </Link>
+        <UserButton />
       </div>
     </div>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
